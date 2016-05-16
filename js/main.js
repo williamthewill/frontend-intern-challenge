8ae7a155beba8ac3d75258f32933ee5b88d89c8b
williamthewill <william-code@hotmail.com>
2016-05-16 18:01:49 -0300

    refatorado arquivo main.js

--- js/main.js
+++ js/main.js
@@ -1,52 +1,58 @@
+/*global $, console*/
+/*jslint plusplus: true */
 function Main() {
-
-    var arrLinks;
-
-    readLinks = $.getJSON("../Assets/urls.json", function () {
-            console.log("success");
-        })
-        .done(function (data) {
+    "use strict";
+    var arrLinks,
+        readLinks = $.getJSON("https://raw.githubusercontent.com/chaordic/frontend-intern-challenge/master/Assets/urls.json", function (data) {
             arrLinks = data;
-        })
-        .fail(function () {
-            console.log("error");
-        })
-
-    // Set another completion function for the request above
-    this.getArrLinks = function () {
+        });
+    this.getLinks = function () {
         readLinks.complete(function () {
             console.log(arrLinks);
         });
-    }
+    };
 
-    this.setHits = function () {
-        readLinks.complete(function () {
-            var hits = 0;
-            for (i = 0; i < 5; i++) {
-                console.log(arrLinks[i]);
-                hits = hits + arrLinks[i].hits;
-            }
-            $(".number").text(hits);
-        });
-    }
+    /**
+     *Method listlinks(), populate list of links in layout
+     **/
     this.listLinks = function () {
         readLinks.complete(function () {
+            var i;
             for (i = 0; i < 5; i++) {
-                var a = $("<a></a>").text(arrLinks[i].shortUrl);
+                var a, span, li;
+                a = $("<a></a>").text(arrLinks[i].shortUrl);
                 a.attr("href", arrLinks[i].url);
                 a.attr("target", "blank");
-                var span = $("<span></span>").text(arrLinks[i].hits);
-                var li = $("<li></li>");
+                span = $("<span></span>").text(arrLinks[i].hits);
+                li = $("<li></li>");
                 li.addClass("link");
                 li.append(a);
                 li.append(span);
                 $(".top-five ul").append(li);
             }
         });
-    }
+    };
+
+    /**
+     *Method setHits(), sum the first five hits and set on element with name class of .number
+     **/
+    this.setHits = function () {
+        readLinks.complete(function () {
+            /*jslint plusplus: true */
+            var i, hits = 0;
+            for (i = 0; i < 5; i++) {
+                hits = hits + arrLinks[i].hits;
+            }
+            $(".hits .number").text(hits);
+        });
+    };
 }
 
+/**
+ *Global scope to application;
+ **/
 $(document).ready(function () {
+    "use strict";
     var d = new Main();
     d.setHits();
     d.listLinks();
